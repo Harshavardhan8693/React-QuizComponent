1d3b72225b6930c748c5c25105c3c0380baf206e
Harshavardhan8693 <harshasmart8693@gmail.com>
2023-04-03 14:28:01 +0530

    Final

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,14 +5,20 @@ import QuizEnd from './QuizEnd.js'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
+
   constructor(props) {
     super(props)
     this.state = { quiz_position: 1 }
   }
   showNextQuestion() {
+    
     this.setState((state) => {
       return { quiz_position: state.quiz_position + 1 }
     })
+
+  }
+  handleResetClick() {
+    this.setState({ quiz_position: 1 })
   }
   render() {
 
@@ -20,7 +26,7 @@ class Quiz extends Component {
 
     return (
       <div>
-        {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+        {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
       </div>
     )
 
